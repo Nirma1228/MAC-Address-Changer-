a6587d7d8c93401baa09a4b51baff195f44c7026
Nirma Nethmini <nirmanethmini@gmail.com>
2025-07-13 19:08:49 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -3,11 +3,6 @@ import re
 import argparse
 
 def get_current_mac(interface):
-import subprocess
-import re
-import argparse
-
-def get_current_mac(interface):
     try:
         result = subprocess.check_output(["ip", "link", "show", interface], encoding="utf-8")
         mac_address = re.search(r"ether ([\w:]+)", result)
